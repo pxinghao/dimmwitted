ef666233840282494ea0314ffe2fe3b812bfb770
Ubuntu <ubuntu@ip-172-31-36-214.ec2.internal>
2015-12-25 12:59:47 +0000

    Modified benchmark

--- run_full_benchmark.py
+++ run_full_benchmark.py
@@ -258,6 +258,6 @@ def draw_all_graphs(load_previous, epoch_range, batch_size_range, thread_range, 
 
 
 #draw_all_graphs(1, [10, 50, 150, 200], [200], [16, 8, 4, 2], [10, 200, 500], [0, 1], ["cyc_movielens_cyc", "cyc_movielens_hog"], 2)
-draw_all_graphs(1, [10, 50, 150], [200], [1, 8], [10, 100], [0, 1], ["cyc_movielens_cyc", "cyc_movielens_hog"], 2)
+draw_all_graphs(0, [10, 50, 150, 200], [200], [1, 8, 16, 32], [10, 100, 200, 500], [0, 1], ["cyc_movielens_cyc", "cyc_movielens_hog"], 2)
 #draw_time_loss_graph(0, 50, [200], [8], [500], [0, 1], ["cyc_movielens_cyc", "cyc_movielens_hog"])
     
